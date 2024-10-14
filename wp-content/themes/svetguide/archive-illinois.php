fee978af21503811afbfd7e484a2ed2bc3d6d5e1
nikhil <nikhilnjk@gmail.com>
2024-10-14 23:15:41 +0530

    finished archive illinois page

--- wp-content/themes/svetguide/archive-illinois.php
+++ wp-content/themes/svetguide/archive-illinois.php
@@ -1,24 +1,264 @@
 <?php
-
 /*
 Template Name: Illinois Archive
 */
-
 ?>
+
 
 <?php get_header(); ?>
 
+<section class="sg-illinois-archive">
 
-<section class="ss-illinois">
-    <div class="container">
-        <h1>this is /illinoise</h1>
-        <h1><?php the_field('title'); ?></h1>
-        <?php the_content(); ?>
+
+    <!-- ads section -->
+
+    <div class="ads-section ">
+        <div class="container">
+            <div class="owl-carousel  owl-theme">
+
+                <?php
+                $image_one = get_option('image_1', '');
+                if ($image_one) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_one) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_two = get_option('image_2', '');
+                if ($image_two) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_two) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_three = get_option('image_3', '');
+                if ($image_three) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_three) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_four = get_option('image_4', '');
+                if ($image_four) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_four) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_five = get_option('image_5', '');
+                if ($image_five) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_five) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_six = get_option('image_6', '');
+                if ($image_six) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_six) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_seven = get_option('image_7', '');
+                if ($image_seven) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_seven) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+                <?php
+                $image_eight = get_option('image_8', '');
+                if ($image_eight) {
+                    echo  ' <div class="item">';
+                    echo '<img src="' . esc_url($image_eight) . '" alt="Image One">';
+                    echo '</div>';
+                }
+                ?>
+
+
+            </div>
+        </div>
     </div>
 
+    <!-- ads section - end -->
+
+
+    <!-- search section -->
+
+    <div class="search-section">
+        <div class="container">
+            <div class="wrapper-1">
+                <h2>Find A Business in Illinois</h2>
+                <div class="input-wrapper">
+                    <img src="/wp-content/themes/svetguide/assets/images/taxonomy-illinois/search-icon.png" alt="">
+                    <input type="text" placeholder="Business name & location in Illinois">
+                </div>
+                <div class="list">
+                </div>
+            </div>
+            <div class="wrapper-2">
+                <p>Do you want your business to be found by the local Eastern European Communities?</p>
+                <a href="/asdf">Contact us</a>
+            </div>
+        </div>
+
+
+    </div>
+
+    <!-- search section - end -->
+
+    <div class="container">
+        <!-- section 1 start -->
+
+        <div class="section-1">
+
+
+
+
+        </div>
+
+        <!-- section 1 end -->
+
+        <div class="browse-by-categories">
+            <img src="/wp-content/themes/svetguide/assets/images/archive-illinois/browse-by-categories.png" alt="">
+            <p>Browse by Categories</p>
+        </div>
+        <!-- section-2 -->
+
+        <div class="section-2">
+
+            <!-- wrapper-1 -->
+
+            <div class="wrapper-1">
+                <h3 class="title">Most Searched</h3>
+                <div class="most-searched-list">
+                    <?php
+                    $most_searched_list = get_option('most_searched_list', '');
+                    if ($most_searched_list) {
+                        echo wp_kses_post($most_searched_list);  // Outputs the WYSIWYG content
+                    }
+                    ?>
+                </div>
+            </div>
+
+            <!-- wrapper-1 end -->
+
+            <!-- wrapper-2 -->
+
+            <div class="wrapper-2">
+
+                <div class="category-list-wrapper">
+                    <div class="category-list">
+                        <p>A</p>
+                    </div>
+                    <div class="category-list">
+                        <p>B</p>
+                    </div>
+                    <div class="category-list">
+                        <p>C</p>
+                    </div>
+                    <div class="category-list">
+                        <p>D</p>
+                    </div>
+                    <div class="category-list">
+                        <p>E</p>
+                    </div>
+                    <div class="category-list">
+                        <p>F</p>
+                    </div>
+                    <div class="category-list">
+                        <p>G</p>
+                    </div>
+                    <div class="category-list">
+                        <p>H</p>
+                    </div>
+                    <div class="category-list">
+                        <p>I</p>
+                    </div>
+                    <div class="category-list">
+                        <p>J</p>
+                    </div>
+                    <div class="category-list">
+                        <p>K</p>
+                    </div>
+                    <div class="category-list">
+                        <p>L</p>
+                    </div>
+                    <div class="category-list">
+                        <p>M</p>
+                    </div>
+                    <div class="category-list">
+                        <p>N</p>
+                    </div>
+                    <div class="category-list">
+                        <p>O</p>
+                    </div>
+                    <div class="category-list">
+                        <p>P</p>
+                    </div>
+                    <div class="category-list">
+                        <p>Q</p>
+                    </div>
+                    <div class="category-list">
+                        <p>R</p>
+                    </div>
+                    <div class="category-list">
+                        <p>S</p>
+                    </div>
+                    <div class="category-list">
+                        <p>T</p>
+                    </div>
+                    <div class="category-list">
+                        <p>U</p>
+                    </div>
+                    <div class="category-list">
+                        <p>V</p>
+                    </div>
+                    <div class="category-list">
+                        <p>W</p>
+                    </div>
+                    <div class="category-list">
+                        <p>X</p>
+                    </div>
+                    <div class="category-list">
+                        <p>Y</p>
+                    </div>
+                    <div class="category-list">
+                        <p>Z</p>
+                    </div>
+                </div>
+
+
+                <div class="wrapper-image">
+                    <a href="/asdf">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/single-illinois/view-pages.png" alt="">
+                    </a>
+                </div>
+
+            </div>
+
+            <!-- end of wrapper-2 -->
+
+        </div>
+
+        <!-- end of section 2 -->
+    </div>
 </section>
 
 
-
+</div>
 
 <?php get_footer(); ?>
--- wp-content/themes/svetguide/js/main.js
+++ wp-content/themes/svetguide/js/main.js
@@ -148,3 +148,108 @@ if (document.querySelector(".sg-illinois-taxonomy")) {
     });
   })();
 }
+
+// archive illinois
+
+if (document.querySelector(".sg-illinois-archive")) {
+  (function () {
+    let listArray = [];
+    let categoryList = document.querySelectorAll(".category-list");
+    categoryList.forEach((item) => {
+      listArray.push(item);
+    });
+
+    async function showItems() {
+      try {
+        const res = await axios.get(
+          `${window.location.origin}/wp-json/wp/v2/il?_fields=link,name`
+        );
+        const dataArr = await res.data;
+
+        for (let i of listArray) {
+          dataArr.forEach((item) => {
+            if (item?.name[0] === i.textContent.trim()[0]) {
+              i.classList.add("show-category-list");
+              let element = document.createElement("div");
+              element.classList.add("category-list-item");
+              element.innerHTML = `<a href="${item?.link}">${item?.name}</a>`;
+              i.appendChild(element);
+            }
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    showItems();
+
+    // carousel
+
+    jQuery(document).ready(function ($) {
+      $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        animateOut: "fadeOut",
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 1,
+          },
+        },
+      });
+    });
+
+    // search function
+
+    let searchBar = document.querySelector(".search-section input");
+    let list = document.querySelector(".search-section .list");
+    let arr = [];
+
+    function listItems(data) {
+      let item = document.createElement("div");
+      item.classList.add(".list-item");
+      item.innerHTML = `<div class="list-item"><a href="${data?.link}">${data?.title?.rendered}</a></div>`;
+      list.append(item);
+    }
+
+    function unlistItem() {
+      document.querySelectorAll(".list-item").forEach((val) => {
+        val.remove();
+      });
+    }
+
+    searchBar.addEventListener("input", async function (e) {
+      arr = [];
+      if (e.target.value.length > 0) {
+        try {
+          let res = await axios(
+            `${window.location.origin}/wp-json/wp/v2/illinois?search=${e.target.value}&_fields=title,link`
+          );
+          let data = await res.data;
+          unlistItem();
+          arr = [...data];
+
+          arr.map((item) => {
+            listItems(item);
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      if (e.target.value.length === 0) {
+        unlistItem();
+      }
+    });
+  })();
+}
