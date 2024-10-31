43ec2d4d6aa30f192c04e448e9f7ea8ca251290a
nikhil <nikhilnjk@gmail.com>
2024-10-31 18:37:33 +0530

    added if condition to load corresponding page css file, added 404 page

---
+++ wp-content/themes/svetguide/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section class="sg-not-found">
+    <div class="container">
+        <h3>404</h3>
+        <p>Page Not Found</p>
+        <a href='/'>Return Home</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
