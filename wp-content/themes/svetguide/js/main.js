e22ff0450078fc00202e764caeb8a7a7b791f407
nikhil <nikhilnjk@gmail.com>
2024-11-01 21:43:24 +0530

    added no results message

--- wp-content/themes/svetguide/js/main.js
+++ wp-content/themes/svetguide/js/main.js
@@ -1,19 +1,199 @@
-// single illinois inner page
+// archive illinois
 
-if (document.querySelector(".sg-illinois-inner")) {
+if (document.querySelector(".sg-illinois-archive")) {
   (function () {
-    let categoryNav = document.querySelector(".ss-illinois-category-name");
-    let pageUrl = window.location.pathname;
-    let pathnameArray = pageUrl.split("/");
-    let categoryName = pathnameArray[2].split("-");
-    let capitalCaseArray = categoryName.map((item) => {
-      return item.charAt(0).toUpperCase() + item.slice(1);
+    let listArray = [];
+    let loader = document.querySelector(".loader");
+    let noResultsFound = document.querySelector(".no-results-found");
+    let categoryList = document.querySelectorAll(".category-list");
+    categoryList.forEach((item) => {
+      listArray.push(item);
     });
-    let combinedName = capitalCaseArray.join(" ");
-    categoryNav.textContent = combinedName;
-    categoryNav.href = `${window.location.origin}/illinois/${pathnameArray[2]}`;
+
+    async function showItems() {
+      try {
+        const res = await axios.get(
+          `${window.location.origin}/wp-json/wp/v2/il?_fields=link,name`
+        );
+        const dataArr = await res.data;
+
+        for (let i of listArray) {
+          dataArr.forEach((item) => {
+            if (item?.name[0] === i.textContent.trim()[0]) {
+              i.classList.add("show-category-list");
+              let element = document.createElement("div");
+              element.classList.add("category-list-item");
+              element.innerHTML = `<a href="${item?.link}">${item?.name}</a>`;
+              i.appendChild(element);
+            }
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    showItems();
+
+    // carousel
+
+    jQuery(document).ready(function ($) {
+      $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        animateOut: "fadeOut",
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 1,
+          },
+        },
+      });
+    });
+
+    // search function
+
+    let searchBar = document.querySelector(".search-section input");
+    let list = document.querySelector(".search-section .list");
+    let arr = [];
+
+    function listItems(data) {
+      list.style.display = "block";
+      let item = document.createElement("div");
+      item.classList.add(".list-item");
+      item.innerHTML = `<div class="list-item"><a href="${data?.link}">${data?.title?.rendered}</a></div>`;
+      list.append(item);
+    }
+
+    function unlistItem() {
+      document.querySelectorAll(".list-item").forEach((val) => {
+        val.remove();
+      });
+      list.style.display = "none";
+    }
+
+    function debounce(func, timeout = 500) {
+      let timer;
+      return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          func.apply(this, args);
+        }, timeout);
+      };
+    }
+
+    async function searching(e) {
+      arr = [];
+      if (e.target.value.trim().length === 0) {
+        noResultsFound.style.display = "none";
+      }
+      if (e.target.value.trim().length > 0) {
+        loader.style.display = "grid";
+        try {
+          let res = await axios(
+            `${
+              window.location.origin
+            }/wp-json/wp/v2/illinois?search=${e.target.value.trim()}&_fields=title,link`
+          );
+          let data = await res.data;
+
+          let tagsRes = await axios(
+            `${window.location.origin}/wp-json/wp/v2/illinois?_fields=acf_fields.search_terms,title,link,acf_fields.address`
+          );
+
+          let tagsData = await tagsRes.data;
+
+          let filteredValue = tagsData.filter((item) => {
+            // Trim the input and split it into an array of words
+            let searchedArray = e.target.value.trimEnd().split(" ");
+
+            // Check if the first word is "service" or "services"
+            const firstWordIsService =
+              searchedArray[0]?.toLowerCase() === "service" ||
+              searchedArray[0]?.toLowerCase() === "services";
+
+            // Case 1: If the first word is "service" or "services", there must be at least two words
+            if (firstWordIsService && searchedArray.length >= 2) {
+              // Ensure all search terms match as whole words
+              return searchedArray.every((val) => {
+                let regex = new RegExp(`\\b${val.toLowerCase()}\\b`, "i");
+                return regex.test(item.acf_fields.search_terms.toLowerCase());
+              });
+            }
+
+            // Case 2: If the first word is not "service" or "services", apply the original filtering logic
+            if (!firstWordIsService) {
+              return searchedArray.every((val) => {
+                let regex = new RegExp(`\\b${val.toLowerCase()}\\b`, "i");
+                let searchTermAndAddress =
+                  item.acf_fields.address.replace(/,/g, "").toLowerCase() +
+                  " " +
+                  item.acf_fields.search_terms.toLowerCase();
+                return regex.test(searchTermAndAddress);
+                // return regex.test(
+
+                // );
+              });
+            }
+
+            // Case 3: Ignore if only "service" or "services" is searched as a single word
+            return false;
+          });
+
+          unlistItem();
+          arr = [...data, ...filteredValue].map((item) => {
+            delete item.acf_fields;
+            return item;
+          });
+
+          let val = Array.from(
+            new Set(arr.map((item) => JSON.stringify(item)))
+          ).map((item) => JSON.parse(item));
+
+          if (val.length === 0 && e.target.value.trim().length > 0) {
+            noResultsFound.style.display = "block";
+          } else {
+            noResultsFound.style.display = "none";
+          }
+
+          val.map((item) => {
+            listItems(item);
+          });
+
+          loader.style.display = "none";
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      if (e.target.value.length === 0) {
+        unlistItem();
+      }
+    }
+
+    searchBar.addEventListener(
+      "input",
+      debounce((e) => searching(e))
+    );
+
+    //redirect to search results page on pressing enter
+    searchBar.addEventListener("keypress", function (e) {
+      if (e.key === "Enter") {
+        window.location = `${window.location.origin}/search-results-illinois/?searchterm=${e.target.value}`;
+      }
+    });
   })();
 }
+
 // taxonomy illinois
 
 if (document.querySelector(".sg-illinois-taxonomy")) {
@@ -22,6 +202,7 @@ if (document.querySelector(".sg-illinois-taxonomy")) {
     let categoryTitle = document.querySelector(".category-heading");
     let subWrapper = document.querySelector(".wrapper-2 .sub-wrapper");
     let loader = document.querySelector(".loader");
+    let noResultsFound = document.querySelector(".no-results-found");
     let pageUrl = window.location.pathname;
     let pathnameArray = pageUrl.split("/");
     let categoryName = pathnameArray[2].split("-");
@@ -148,6 +329,9 @@ if (document.querySelector(".sg-illinois-taxonomy")) {
 
     async function searching(e) {
       arr = [];
+      if (e.target.value.trim().length === 0) {
+        noResultsFound.style.display = "none";
+      }
       if (e.target.value.trim().length > 0) {
         loader.style.display = "grid";
         try {
@@ -210,6 +394,10 @@ if (document.querySelector(".sg-illinois-taxonomy")) {
           let val = Array.from(
             new Set(arr.map((item) => JSON.stringify(item)))
           ).map((item) => JSON.parse(item));
+
+          if (val.length === 0) {
+            noResultsFound.style.display = "block";
+          }
 
           val.map((item) => {
             listItems(item);
@@ -238,189 +426,20 @@ if (document.querySelector(".sg-illinois-taxonomy")) {
   })();
 }
 
-// archive illinois
+// single illinois inner page
 
-if (document.querySelector(".sg-illinois-archive")) {
+if (document.querySelector(".sg-illinois-inner")) {
   (function () {
-    let listArray = [];
-    let loader = document.querySelector(".loader");
-    let categoryList = document.querySelectorAll(".category-list");
-    categoryList.forEach((item) => {
-      listArray.push(item);
+    let categoryNav = document.querySelector(".ss-illinois-category-name");
+    let pageUrl = window.location.pathname;
+    let pathnameArray = pageUrl.split("/");
+    let categoryName = pathnameArray[2].split("-");
+    let capitalCaseArray = categoryName.map((item) => {
+      return item.charAt(0).toUpperCase() + item.slice(1);
     });
-
-    async function showItems() {
-      try {
-        const res = await axios.get(
-          `${window.location.origin}/wp-json/wp/v2/il?_fields=link,name`
-        );
-        const dataArr = await res.data;
-
-        for (let i of listArray) {
-          dataArr.forEach((item) => {
-            if (item?.name[0] === i.textContent.trim()[0]) {
-              i.classList.add("show-category-list");
-              let element = document.createElement("div");
-              element.classList.add("category-list-item");
-              element.innerHTML = `<a href="${item?.link}">${item?.name}</a>`;
-              i.appendChild(element);
-            }
-          });
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    showItems();
-
-    // carousel
-
-    jQuery(document).ready(function ($) {
-      $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        dots: false,
-        animateOut: "fadeOut",
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: false,
-        responsive: {
-          0: {
-            items: 1,
-          },
-          600: {
-            items: 1,
-          },
-          1000: {
-            items: 1,
-          },
-        },
-      });
-    });
-
-    // search function
-
-    let searchBar = document.querySelector(".search-section input");
-    let list = document.querySelector(".search-section .list");
-    let arr = [];
-
-    function listItems(data) {
-      list.style.display = "block";
-      let item = document.createElement("div");
-      item.classList.add(".list-item");
-      item.innerHTML = `<div class="list-item"><a href="${data?.link}">${data?.title?.rendered}</a></div>`;
-      list.append(item);
-    }
-
-    function unlistItem() {
-      document.querySelectorAll(".list-item").forEach((val) => {
-        val.remove();
-      });
-      list.style.display = "none";
-    }
-
-    function debounce(func, timeout = 500) {
-      let timer;
-      return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-          func.apply(this, args);
-        }, timeout);
-      };
-    }
-
-    async function searching(e) {
-      arr = [];
-      if (e.target.value.trim().length > 0) {
-        loader.style.display = "grid";
-        try {
-          let res = await axios(
-            `${
-              window.location.origin
-            }/wp-json/wp/v2/illinois?search=${e.target.value.trim()}&_fields=title,link`
-          );
-          let data = await res.data;
-
-          let tagsRes = await axios(
-            `${window.location.origin}/wp-json/wp/v2/illinois?_fields=acf_fields.search_terms,title,link,acf_fields.address`
-          );
-
-          let tagsData = await tagsRes.data;
-
-          let filteredValue = tagsData.filter((item) => {
-            // Trim the input and split it into an array of words
-            let searchedArray = e.target.value.trimEnd().split(" ");
-
-            // Check if the first word is "service" or "services"
-            const firstWordIsService =
-              searchedArray[0]?.toLowerCase() === "service" ||
-              searchedArray[0]?.toLowerCase() === "services";
-
-            // Case 1: If the first word is "service" or "services", there must be at least two words
-            if (firstWordIsService && searchedArray.length >= 2) {
-              // Ensure all search terms match as whole words
-              return searchedArray.every((val) => {
-                let regex = new RegExp(`\\b${val.toLowerCase()}\\b`, "i");
-                return regex.test(item.acf_fields.search_terms.toLowerCase());
-              });
-            }
-
-            // Case 2: If the first word is not "service" or "services", apply the original filtering logic
-            if (!firstWordIsService) {
-              return searchedArray.every((val) => {
-                let regex = new RegExp(`\\b${val.toLowerCase()}\\b`, "i");
-                let searchTermAndAddress =
-                  item.acf_fields.address.replace(/,/g, "").toLowerCase() +
-                  " " +
-                  item.acf_fields.search_terms.toLowerCase();
-                return regex.test(searchTermAndAddress);
-                // return regex.test(
-
-                // );
-              });
-            }
-
-            // Case 3: Ignore if only "service" or "services" is searched as a single word
-            return false;
-          });
-
-          unlistItem();
-          arr = [...data, ...filteredValue].map((item) => {
-            delete item.acf_fields;
-            return item;
-          });
-
-          let val = Array.from(
-            new Set(arr.map((item) => JSON.stringify(item)))
-          ).map((item) => JSON.parse(item));
-
-          val.map((item) => {
-            listItems(item);
-          });
-
-          loader.style.display = "none";
-        } catch (err) {
-          console.error(err);
-        }
-      }
-      if (e.target.value.length === 0) {
-        unlistItem();
-      }
-    }
-
-    searchBar.addEventListener(
-      "input",
-      debounce((e) => searching(e))
-    );
-
-    //redirect to search results page on pressing enter
-    searchBar.addEventListener("keypress", function (e) {
-      if (e.key === "Enter") {
-        window.location = `${window.location.origin}/search-results-illinois/?searchterm=${e.target.value}`;
-      }
-    });
+    let combinedName = capitalCaseArray.join(" ");
+    categoryNav.textContent = combinedName;
+    categoryNav.href = `${window.location.origin}/illinois/${pathnameArray[2]}`;
   })();
 }
 
@@ -434,6 +453,7 @@ if (document.querySelector(".sg-search-results-illinois")) {
     let loader = document.querySelector(".loader");
     let searchBar = document.querySelector(".search-section input");
     let list = document.querySelector(".search-section .list");
+    let noResultsFound = document.querySelector(".no-results-found");
     let noResultsElement = document.querySelector(".message");
     let query = document.querySelector(".query");
     let arr = [];
@@ -466,6 +486,9 @@ if (document.querySelector(".sg-search-results-illinois")) {
 
     async function searching(e) {
       arr = [];
+      if (e.target.value.trim().length === 0) {
+        noResultsFound.style.display = "none";
+      }
       if (e.target.value.trim().length > 0) {
         loader.style.display = "grid";
         try {
@@ -528,6 +551,10 @@ if (document.querySelector(".sg-search-results-illinois")) {
           let val = Array.from(
             new Set(arr.map((item) => JSON.stringify(item)))
           ).map((item) => JSON.parse(item));
+
+          if (val.length === 0) {
+            noResultsFound.style.display = "block";
+          }
 
           val.map((item) => {
             listItems(item);
